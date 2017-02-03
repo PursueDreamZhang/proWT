a7c0bb00b86d060995afafde12a328c1b6821c45
PursueDreamZhang <pursuedreamzhang@foxmail.com>
2017-02-03 23:42:11 +0800

    222
    222

---
+++ WeiTengApp/app/src/main/java/com/weiteng/weitengapp/bean/NoticeBean.java
@@ -0,0 +1,12 @@
+package com.weiteng.weitengapp.bean;
+
+/**
+ * Created by Admin on 2017/1/18.
+ */
+
+public class NoticeBean {
+    public String url;
+    public String img;
+    public String title;
+    public String createTime;
+}
